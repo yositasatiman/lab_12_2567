3678ab76d0c95143b24e8b792f965f4c08219854
Yosita satiman <yosita_satiman@cmu.ac.th>
2024-12-24 14:40:43 +0700

    lab_12_2

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
+#include <numeric>
 using namespace std;
+
+int gcd(int x, int y);
 
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int x, int y){
+    if(x%y == 0){
+        return y;
+    }else{
+        return gcd(y, x%y);
+    }
+
 }
