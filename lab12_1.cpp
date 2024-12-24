3f7a3e66d548289f943a59b05e62226a9d749c49
Yosita satiman <yosita_satiman@cmu.ac.th>
2024-12-24 14:31:17 +0700

    lab12_1

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std;
 
+long long int fibonacci(int x);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
 }
+
+long long int fibonacci(int x){
+    if(x == 0){
+        return 0;
+    }else if(x == 1){
+        return 1;
+    }else if(x > 1){
+        return fibonacci(x-1) + fibonacci(x-2);
+    }
+    return x;
+}
+
